86322a95402a588c637290f498f9321fff01f1af
Juan <j.villagran03@ufromail.cl>
2026-01-13 17:04:04 -0300

    Commit de lanzamiento

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Repositorio',
+    version='0.1',
+    packages=find_packages(),
+    author='Juan Villagran',
+    author_email='juan.villagran@insodev.cl',
+    url='https://github.com/JuanVillagranOlivera/Repositorio',
+)
