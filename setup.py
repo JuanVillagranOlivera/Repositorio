4b204866d9a91d002b1c5b085dbe2fee0ad4abf7
JuanVillagranOlivera <juan.villagran@insodev.cl>
2026-01-13 17:20:39 -0300

    Correcion de escritura en la version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Repositorio',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(),
     author='Juan Villagran',
     author_email='juan.villagran@insodev.cl',
